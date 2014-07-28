50b1ef55ddfcf51ca5c57a804027386db1961b3c
vcrobotics <aloha1epp@yahoo.com>
2014-07-28 19:19:36 +0000

    file upload

--- github.php
+++ github.php
@@ -1,4 +1,11 @@
+<html>
+<form name="input" action="gitpull.php" method="get">
+<input type="submit" value="Upload Code">
+</form>
 <?php
-`git pull`;
-print "test";
+if($_SESSION['success']==1){
+	echo "success";
+	$_SESSION['success']=0;
+}
 ?>
+</html>
