46426c9584ed2935cf4f1cc0e16916047d984825
apache8080 <desai.rishi1@gmail.com>
2014-08-06 21:03:37 -0700

    fixed joystick.py

--- tegrabot/joystick.py
+++ tegrabot/joystick.py
@@ -11,25 +11,25 @@ class Joystick:
     def __init__(self, s=None):
         if s is None:
             self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.s.bind("", port)
+            self.s.bind(("", Joystick.port))
         else:
             self.s = s
     def connect(self):
         data, addr = self.s.recvfrom(1024)
-        sessionStarted = pickle.load(data)
+        Joystick.sessionStarted = pickle.load(data)
     def isConnected():
-        return sessionStarted
+        return Joystick.sessionStarted
     def setData(self):
         data, addr = self.s.recvfrom(1024)
-        joyData = pickle.load(data)
+        Joystick.joyData = pickle.load(data)
 
     def getJoyOneYAxis():
         #data, addr = s.recvfrom(1024)
-        return joyData[1]
+        return Joystick.joyData[1]
     def getJoyTwoYAxis():
         #data, addr = s.recvfrom(1024)
-        return joyData[2] 
+        return Joystick.joyData[2] 
     def getButtonVal():
         #data, addr = s.recvfrom(1024)
-        return joyData[3]
+        return Joystick.joyData[3]
     
